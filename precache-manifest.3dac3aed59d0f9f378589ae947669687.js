378467cbeca70927ccefac2b29b6c06165847bc4
k3ndr1c <ken_dric@hotmail.com>
2020-08-05 16:35:26 -0400

    Updates

---
+++ precache-manifest.3dac3aed59d0f9f378589ae947669687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7738250abb11b72936a3816b28088a8",
+    "url": "/kanagana/index.html"
+  },
+  {
+    "revision": "5b89d620ea6b77080c5b",
+    "url": "/kanagana/static/css/main.7df9e20d.chunk.css"
+  },
+  {
+    "revision": "dff334bc10fa5ab39730",
+    "url": "/kanagana/static/js/2.add88060.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanagana/static/js/2.add88060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b89d620ea6b77080c5b",
+    "url": "/kanagana/static/js/main.995b44aa.chunk.js"
+  },
+  {
+    "revision": "fdd0475a80811a529033",
+    "url": "/kanagana/static/js/runtime-main.8dca3fd6.js"
+  },
+  {
+    "revision": "64aa3083ffd59fa79a6ae514e530267d",
+    "url": "/kanagana/static/media/paper-background.64aa3083.jpg"
+  }
+]);
